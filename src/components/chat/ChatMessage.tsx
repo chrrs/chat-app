60feb20337a1e0935dae786696d3db6ef2335fb6
chrrs <git@chrr.me>
2025-05-05 18:00:35 +0200

    feat: show reply messages

--- src/components/chat/ChatMessage.tsx
+++ src/components/chat/ChatMessage.tsx
@@ -1,5 +1,6 @@
+import { Colors } from "@/lib/constants/Colors";
 import type { Event } from "@/lib/irc/chat";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import { InlineMessage } from "./InlineMessage";
 
 interface Props {
@@ -9,6 +10,12 @@ interface Props {
 export const ChatMessage = ({ event }: Props) => {
 	return (
 		<View style={styles.root}>
+			{event.replyTo && (
+				<Text style={styles.parent} numberOfLines={1}>
+					<Text style={styles.parentAuthor}>{`${event.replyTo.author.name}: `}</Text>
+					{event.replyTo.text}
+				</Text>
+			)}
 			<InlineMessage message={event.message} />
 		</View>
 	);
@@ -18,5 +25,15 @@ const styles = StyleSheet.create({
 	root: {
 		paddingHorizontal: 12,
 		paddingVertical: 3,
+	},
+
+	parent: {
+		color: Colors.mutedText,
+		fontSize: 12,
+		marginLeft: 8,
+	},
+
+	parentAuthor: {
+		fontWeight: "bold",
 	},
 });
